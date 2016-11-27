d28480c70137324c413a73b6ed65df4c39c58701
Amy Yang <jy152@duke.edu>
2016-11-27 16:26:47 -0500

    debug pipe

--- core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
+++ core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
@@ -273,19 +273,19 @@ private[spark] class CoarseGrainedExecutorBackend(
     val NUMCPU = 8;
     var process = ""
 
-    val myStr = "top -n 1 -b -p "+processID
-    val topout2 = myStr #| "tail -2" #| "head -1" !!
-    val topout = topout2.trim.split(" +")
+    val myStr = "top -n 1 -b -p "+processID !!
+    // val topout2 = myStr #| "tail -2" #| "head -1" !!
+    // val topout = topout2.trim.split(" +")
 
     // val topout = "top -n 1 -b -p "+ 5096 + " | tail -2 | head -1" !!
       // Seq("/bin/sh", " -c ", "\"top -n 1 -b -p " + processID + " | tail -2 | head -1\"").!!.trim.split(" +")
 
-    val len = topout(5).length
-    if(topout(5).endsWith("g")) { memory = 1024L*1024L*1024L*topout(5).take(len-1).toDouble }
-    else if(topout(5).endsWith("m")) { memory = 1024L*1024L*topout(5).take(len-1).toDouble }
-    else if(topout(5).endsWith("k")) { memory = 1024L*topout(5).take(len-1).toDouble }
-    else { memory = topout(5).toDouble }
-    CPU = topout(8).toDouble / NUMCPU
+    // val len = topout(5).length
+    // if(topout(5).endsWith("g")) { memory = 1024L*1024L*1024L*topout(5).take(len-1).toDouble }
+    // else if(topout(5).endsWith("m")) { memory = 1024L*1024L*topout(5).take(len-1).toDouble }
+    // else if(topout(5).endsWith("k")) { memory = 1024L*topout(5).take(len-1).toDouble }
+    // else { memory = topout(5).toDouble }
+    // CPU = topout(8).toDouble / NUMCPU
 
     return (memory, CPU)
   }
