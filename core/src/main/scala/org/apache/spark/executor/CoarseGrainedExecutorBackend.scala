3f4b8e4e5f9b23eaa968521d6273e119641d3c40
Amy Yang <jy152@duke.edu>
2016-11-27 11:43:46 -0500

    trying to fix SparkEnv

--- core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
+++ core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
@@ -273,7 +273,7 @@ private[spark] class CoarseGrainedExecutorBackend(
     val NUMCPU = 8;
     var process = ""
 
-    val topout = Seq("/bin/sh", "-c", "top -n 1 -b -p " + processID + " | grep "+processID + " | tail -1").!!.trim.split(" +")
+    val topout = Seq("/bin/sh", "-c", "top -n 1 -b -p " + processID + " | tail -2 | head -1").!!.trim.split(" +")
 
     val len = topout(5).length
     if(topout(5).endsWith("g")) { memory = 1024L*1024L*1024L*topout(5).take(len-1).toDouble }
